f540925cadc0f7745240bfb10d60be0ed879ab8d
Daniel Monge <damonro@hotmail.com>
2023-04-28 03:14:07 -0600

    adding last details

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 describe('Practice Test', () => {
-/*  it('Visits and validates the dropdown option', () => {
+  it('Visits and validates the dropdown option', () => {
     cy.visit('http://the-internet.herokuapp.com')
     cy.contains('Dropdown').click()
     cy.url().should('include', '/dropdown')
@@ -12,16 +12,23 @@ describe('Practice Test', () => {
     cy.url().should('include', '/basic_auth')
     cy.get('h3').should('include.text', 'Basic Auth')
     cy.get('p').should('include.text', 'Congratulations! You must have the proper credentials.')
-  }) */ 
+  })  
   it('Visits the Sortable Data Tables option', () => {
     cy.visit('http://the-internet.herokuapp.com')
     cy.contains('Sortable Data Tables').click()
     cy.url().should('include', '/tables')
     cy.get('#table2').scrollIntoView()
+    cy.get('#table2 thead tr th').should('have.length', '6')
     cy.contains('.dues', 'Due').click()
-    //verify values are sorted Mayor a menor
-    cy.contains('.header headerSortUp', 'Due')
-    //cy.get('table#table1 tbody td')
+    cy.contains('.dues', 'Due').click()
+    cy.get('#table2').contains('th', 'Due')
+    cy.contains('#table2 td','$51.00').should('exist')
+    cy.get('#table2 tbody tr').eq(1).within(() =>{
+      cy.get('td').each(($el) =>{
+        if ($el.text() == '$51.00'){
+          cy.log('column sorted')
+        }
+      })
+    })
   })
-
 })
